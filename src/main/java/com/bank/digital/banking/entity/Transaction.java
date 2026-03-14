ff8cd2ab95493921f62f89d90f4e1d50cf787d6f
Owais Ali Khan <fazebkhan1@gmail.com>
2026-03-14 16:52:46 +0530

    Update Transaction.java

--- src/main/java/com/bank/digital/banking/entity/Transaction.java
+++ src/main/java/com/bank/digital/banking/entity/Transaction.java
@@ -4,7 +4,7 @@ package com.bank.digital.banking.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor; 
 
 import java.time.LocalDateTime;
 
