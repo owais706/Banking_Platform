53f8a89a47fe83fa3980f80da857630791c18cdc
Owais Ali Khan <fazebkhan1@gmail.com>
2026-03-14 16:52:28 +0530

    Update Account.java
    Updated Entity file

--- src/main/java/com/bank/digital/banking/entity/Account.java
+++ src/main/java/com/bank/digital/banking/entity/Account.java
@@ -1,6 +1,6 @@
 package com.bank.digital.banking.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty; 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
