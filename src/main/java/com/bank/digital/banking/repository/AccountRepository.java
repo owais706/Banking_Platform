95ed8dd3e4901f0e941fd5072aad98c4f08df47f
Owais Ali Khan <fazebkhan1@gmail.com>
2026-03-14 16:53:12 +0530

    Update AccountRepository.java
    Updated new feature

--- src/main/java/com/bank/digital/banking/repository/AccountRepository.java
+++ src/main/java/com/bank/digital/banking/repository/AccountRepository.java
@@ -3,6 +3,6 @@ package com.bank.digital.banking.repository;
 import com.bank.digital.banking.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> { 
 
 }
