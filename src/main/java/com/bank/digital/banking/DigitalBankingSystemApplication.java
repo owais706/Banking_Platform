bb8f60ab5c0fac504becb15e9a698755bc7c525c
Owais Ali Khan <fazebkhan1@gmail.com>
2026-03-14 16:54:10 +0530

    Update DigitalBankingSystemApplication.java
    Updated new feature

--- src/main/java/com/bank/digital/banking/DigitalBankingSystemApplication.java
+++ src/main/java/com/bank/digital/banking/DigitalBankingSystemApplication.java
@@ -1,6 +1,6 @@
 package com.bank.digital.banking;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
