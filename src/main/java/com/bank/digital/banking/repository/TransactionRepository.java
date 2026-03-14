fc5fcb4abce41e07367cf9f64b455abf1335811c
Owais Ali Khan <fazebkhan1@gmail.com>
2026-03-14 16:53:38 +0530

    Update TransactionRepository.java
    Updated new feature

--- src/main/java/com/bank/digital/banking/repository/TransactionRepository.java
+++ src/main/java/com/bank/digital/banking/repository/TransactionRepository.java
@@ -2,7 +2,7 @@ package com.bank.digital.banking.repository;
 
 import com.bank.digital.banking.entity.Account;
 import com.bank.digital.banking.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; 
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
